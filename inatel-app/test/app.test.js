fefbc0df78fa85cc6f375bee3ddc015246775d4d
MarceloAbrantes <marcelo.abrantes@gec.inatel.br>
2024-05-10 02:15:20 -0300

    Teste verifica usuario

---
+++ inatel-app/test/app.test.js
@@ -0,0 +1,22 @@
+// app.test.js
+const request = require('supertest');
+// Importa os dados de usuários do módulo '../db'
+const users = require('../db');
+
+
+test('verifica um usuario valido', () => {
+    const username = 'admin';
+    const password = 'admin';
+    const user = users.find(u => u.username === username && u.password === password);
+    
+    expect(user).toBeDefined();
+});
+
+test('verifica um usuario invalido', () => {
+    const username = 'usuario_invalido';
+    const password = 'senha_invalida';
+    const user = users.find(u => u.username === username && u.password === password);
+    
+    expect(user).toBeUndefined();
+});
+  
