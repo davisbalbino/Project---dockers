8c873098cf57fad492a78625f67cc0d442d2c70b
MarceloAbrantes <marcelo.abrantes@gec.inatel.br>
2024-05-25 22:58:45 -0300

    alteracao no teste

---
+++ inatel-app/src/tests/app.test.js
@@ -0,0 +1,21 @@
+// app.test.js
+// Importa os dados de usuários do módulo '../db'
+const users = require('../../db');
+
+
+test('verifica um usuario valido', () => {
+    const username = 'admin';
+    const password = 'admin';
+    const user = users.find(u => u.username === username && u.password === password);
+    
+    expect(user).toBeDefined();
+});
+
+test('verifica um usuario invalido', () => {
+    const username = 'usuario_invalido';
+    const password = 'senha_invalida';
+    const user = users.find(u => u.username === username && u.password === password);
+    
+    expect(user).toBeUndefined();
+});
+  
