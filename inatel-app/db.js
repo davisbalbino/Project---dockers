6b140c25dde7edbfba12b0336d4c12f75c6482bc
davisbalbino <davidossantosbalbinomarcelino@gmail.com>
2024-06-10 12:16:53 -0300

    modificações para susar docker e o compose

--- inatel-app/db.js
+++ inatel-app/db.js
@@ -1,6 +1,45 @@
-const users = [
-    { username: 'admin', password: 'admin' },
-    { username: 'user', password: '123456' }
+const { MongoClient } = require('mongodb');
+
+const MONGODB_URI = 'mongodb://localhost:27017';
+const DB_NAME = 'test';
+const COLLECTION_NAME = 'users';
+
+// Dados dos usuários
+const usersData = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' }
 ];
 
-module.exports = users;
+// Função para inserir os usuários no banco de dados
+async function insertUsers() {
+    let client; // Declaração da variável client fora do bloco try
+
+    try {
+        // Conectar ao MongoDB
+        client = new MongoClient(MONGODB_URI, { useUnifiedTopology: true });
+        await client.connect();
+        console.log("Conectado ao servidor MongoDB");
+
+        // Selecionar o banco de dados
+        const db = client.db(DB_NAME);
+
+        // Selecionar a coleção
+        const collection = db.collection(COLLECTION_NAME);
+
+        // Inserir os usuários na coleção
+        await collection.insertMany(usersData);
+        console.log("Usuários inseridos com sucesso");
+
+    } catch (error) {
+        console.error("Erro ao inserir usuários:", error);
+    } finally {
+        if (client) {
+            // Fechar a conexão com o cliente
+            await client.close();
+            console.log("Conexão com o MongoDB fechada");
+        }
+    }
+}
+
+// Chamar a função para inserir os usuários
+insertUsers();
--- inatel-app/server.js
+++ inatel-app/server.js
@@ -1,33 +1,49 @@
 const express = require('express');
-const cors = require('cors'); // Importe o pacote cors
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = 'mongodb://localhost:27017';
+const DB_NAME = 'test';
+const COLLECTION_NAME = 'users';
 
-// Middleware para permitir o uso de JSON nas requisições
 app.use(express.json());
-
-// Use o middleware cors para permitir solicitações de diferentes origens
-app.use(cors(
-    {
-        origin: 'http://localhost:3000' // Permite apenas solicitações vindas de http://localhost:3000
-      }
-));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 // Endpoint para autenticação de login
-const users = require('./db');
-
-app.post('/api/login', (req, res) => {
+app.post('/api/login', async (req, res) => {
     const { username, password } = req.body;
-    console.log('Credenciais recebidas:', username, password); 
-    const user = users.find(u => u.username === username && u.password === password);
-    console.log('Usuário encontrado:', user); 
-    if (user) {
-        res.json({ success: true, message: 'Login bem-sucedido' });
-    } else {
-        res.status(401).json({ success: false, message: 'Credenciais inválidas' });
-    }
+    console.log('Credenciais recebidas:', username, password);
+
+    try {
+        // Conectar ao MongoDB
+        const client = new MongoClient(MONGODB_URI, { useUnifiedTopology: true });
+        await client.connect();
+        console.log("Conectado ao servidor MongoDB");
+
+        // Selecionar o banco de dados
+        const db = client.db(DB_NAME);
+
+        // Selecionar a coleção
+        const collection = db.collection(COLLECTION_NAME);
+
+        // Consultar o banco de dados para encontrar o usuário
+        const user = await collection.findOne({ username, password });
+
+        if (user) {
+            res.json({ success: true, message: 'Login bem-sucedido' });
+        } else {
+            res.status(401).json({ success: false, message: 'Credenciais inválidas' });
+        }
+    } catch (error) {
+        console.error("Erro ao autenticar usuário:", error);
+        res.status(500).json({ success: false, message: 'Erro interno do servidor' });
+    } 
 });
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
